36d20bd5dc30b80fa34a79643229411e1134f7bc
seon-mikim <tjsalsun@gmail.com>
2023-07-11 02:07:03 +0900

    feat: post api 생성 및 get api 생성
    - class Memo 객체 생성(pydantic BaseModel를 통해서)
    - post api 생성
    - get api 생성

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
+memos = []
 app = FastAPI()
+
+class Memo(BaseModel):
+  id: int
+  content: str
+  
+@app.post('/memos')
+def create_memo(memo:Memo):
+  memos.append(memo)
+  return '메모 추가에 성공했습니다.'
+
+@app.get('/memos')
+def get_memos():
+  return memos
+  
 
 app.mount('/',StaticFiles(directory='static', html='True'), name='static')
